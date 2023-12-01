4e5cb28bd38f26432c00b3e7d20a67826236327a
Luis Silva <ifaf.silva12@gmail.com>
2023-12-01 12:17:27 +0000

    c

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,63 @@
 import { Nav, Navbar, NavLink, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { userSession, authenticate, verifyUser, login} from '../auth';
+import {userSession, authenticate, login, verifyToken} from '../auth';
+import {useCookies} from "react-cookie";
+import Button from "react-bootstrap/Button";
+import {useEffect, useState} from "react";
+import '../App.css';
 
 const Navigation = () => {
+
+    const [cookies, removeCookie] = useCookies(['auth']);
+    const [isUserVerified, setIsUserVerified] = useState(false);
+    function aaa() {
+        console.log("A.a.a")
+    }
+
+    const handleSignOut = () => {
+        // Perform sign-out logic
+        signOut();
+
+        // Redirect to the home page
+        window.location.href = '/';
+    };
+    function signOut() {
+        removeCookie('auth');
+    }
+    useEffect(() => {
+        const checkUserVerification = async () => {
+            if (cookies.auth) {
+                let length = cookies.auth.length;
+
+                if (length > 20) {
+                    const info = await verifyToken(cookies.auth);
+
+                    if (info.isValid) {
+                        console.log("Token is Valid");
+                        setIsUserVerified(true);
+                        return true
+                    } else {
+                        console.log("Error in validation");
+                        setIsUserVerified(false);
+                        return false
+                    }
+                } else {
+                    console.log('Token length is not greater than 20');
+                    setIsUserVerified(false);
+                    return false
+                }
+            } else {
+                console.log("token cookie is missing");
+                setIsUserVerified(false);
+                return false
+            }
+        };
+
+        checkUserVerification().then(result => {
+            console.log('Result from checkUserVerification:', result);
+        });
+    }, [cookies.auth]);
+
     return (
             <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
                 <div className="container">
@@ -13,10 +68,17 @@ const Navigation = () => {
                             <NavLink  eventKey="2" as={Link} to="/movies">Movies</NavLink>
                             <NavLink  eventKey="3" as={Link} to="/users">Users</NavLink>
                             <NavLink  eventKey="4" as={Link} to="/map">Map</NavLink>
-                            {!verifyUser() &&
-                                <NavLink  eventKey="5" as={Link} to="/login">Login</NavLink>
+                            {!isUserVerified && (
+                                <NavLink eventKey="5" as={Link} to="/login" className="login-link">
+                                    Login
+                                </NavLink>
+                            )}
+                            {isUserVerified && (
+                                <Button onClick={handleSignOut} variant="outline-light">
+                                    Sign Out
+                                </Button>
+                            )}
 
-                            }
 
                         </Nav>
                     </Navbar.Collapse>
@@ -32,7 +94,7 @@ const Navigation = () => {
                         </a>
 
                     }
-                </div>  
+                </div>
             </Navbar>
     );
 }

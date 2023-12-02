e51904d1175591754a7e5e1ac7d00664a86ac821
Luis Silva <ifaf.silva12@gmail.com>
2023-12-02 19:16:09 +0000

    c

--- src/components/CinemaMap.js
+++ src/components/CinemaMap.js
@@ -4,6 +4,8 @@ import { GoogleMap, Marker, InfoWindow, LoadScript } from '@react-google-maps/ap
 const CinemaMap = () => {
     const [cinemas, setCinemas] = useState([]);
     const [selectedCinema, setSelectedCinema] = useState(null);
+    const [mapLoaded, setMapLoaded] = useState(false);
+    const [retryCount, setRetryCount] = useState(0);
 
     useEffect(() => {
         const fetchCinemas = async () => {
@@ -13,11 +15,17 @@ const CinemaMap = () => {
                 setCinemas(data);
             } catch (error) {
                 console.error('Error fetching cinemas:', error);
+                handleLoadError();
             }
         };
 
         fetchCinemas();
-    }, []);
+
+        return () => {
+            // Cleanup function to unload the Google Maps API when the component is unmounted
+            setMapLoaded(false);
+        };
+    }, []); // Empty dependency array to run the effect only once on mount
 
     const center = { lat: 38.736946, lng: -9.142685 };
 
@@ -25,47 +33,87 @@ const CinemaMap = () => {
         height: '400px',
         width: '100%',
     };
+    const infoWindowStyle = {
+        width: '220px', // Set the width to match the desired width of the image
+    };
+    const onLoad = () => {
+        setMapLoaded(true);
+    };
+
+    const onError = () => {
+        console.error('Error loading Google Maps API');
+        if (retryCount < 5) {
+            // Retry up to 5 times (adjust as needed)
+            setRetryCount(retryCount + 1);
+            setMapLoaded(false); // Reset mapLoaded state for the retry
+        } else {
+            console.error('Exceeded maximum retry attempts. Reloading the page...');
+            handleLoadError();
+        }
+    };
+
+    const handleLoadError = () => {
+        // Reload the page
+        setTimeout(() => {
+            window.location.reload();
+        }, 3000); // You can adjust the delay (in milliseconds) before reloading
+    };
 
     return (
-        <LoadScript
-            googleMapsApiKey="AIzaSyBnRw6pPuuCuz0Kh4MU_YNb02pOc82U56o"
-        >
-            <GoogleMap
-                mapContainerStyle={mapContainerStyle}
-                center={center}
-                zoom={12}
+        <div>
+            <LoadScript
+                googleMapsApiKey="AIzaSyBnRw6pPuuCuz0Kh4MU_YNb02pOc82U56o"
+                onLoad={onLoad}
+                onError={onError}
             >
-                {cinemas.map((cinema) => (
-                    <Marker
-                        key={cinema._id}
-                        position={{ lat: cinema.y, lng: cinema.x }}
-                        icon={{
-                            url: '../cinema.png',
-                            scaledSize: new window.google.maps.Size(40, 40),
-                        }}
-                        onClick={() => {
-                            setSelectedCinema(cinema);
-                        }}
+                {mapLoaded ? (
+                    <GoogleMap
+                        mapContainerStyle={mapContainerStyle}
+                        center={center}
+                        zoom={12}
                     >
-                        {selectedCinema === cinema && (
-                            <InfoWindow
-                                position={{ lat: selectedCinema.y, lng: selectedCinema.x }}
-                                onCloseClick={() => {
-                                    setSelectedCinema(null);
-
+                        {cinemas.map((cinema) => (
+                            <Marker
+                                key={cinema._id}
+                                position={{ lat: cinema.y, lng: cinema.x }}
+                                icon={{
+                                    url: '../cinema.png',
+                                    scaledSize: new window.google.maps.Size(40, 40),
+                                }}
+                                onClick={() => {
+                                    setSelectedCinema(cinema);
                                 }}
                             >
-                                <div>
-                                    <h5>{selectedCinema.name}</h5>
-                                    {/* Add additional information here if needed */}
-                                </div>
-                            </InfoWindow>
-                        )}
-                    </Marker>
-                ))}
-            </GoogleMap>
-        </LoadScript>
+                                {selectedCinema === cinema && (
+                                    <InfoWindow
+                                        position={{ lat: selectedCinema.y, lng: selectedCinema.x }}
+                                        onCloseClick={() => {
+                                            setSelectedCinema(null);
+                                        }}
+                                    >
+                                        <div className="info-window">
+                                            <h3>{selectedCinema.name} </h3>
+                                            {selectedCinema.imageUrl && (
+                                                <img
+                                                    src={selectedCinema.imageUrl}
+                                                    alt={selectedCinema.name}
+                                                    style={{ width: '200px', height: 'auto' }}
+                                                />
+                                            )}
+                                            {/* Add additional information here if needed */}
+                                        </div>
+                                    </InfoWindow>
+                                )}
+                            </Marker>
+                        ))}
+                    </GoogleMap>
+                ) : (
+                    <div>Loading...</div>
+                )}
+            </LoadScript>
+        </div>
     );
 };
 
 export default CinemaMap;
+
--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import CinemaMap from '../components/CinemaMap';
 
-export default function App() {
+const App = () => {
     return (
         <div className="container pt-5 pb-5">
             <h2>Cinemas Map</h2>
@@ -10,4 +10,4 @@ export default function App() {
     );
 };
 
-
+export default App;

3ed8141ae6f6005af9b0b811477404fb2727114b
Pedro Santos <pedrojgmsantos@gmail.com>
2023-12-02 00:47:28 +0000

    c

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -5,20 +5,16 @@ import {
   MDBCardTitle,
   MDBCardSubTitle,
   MDBCardText,
-  MDBCardLink
-} from 'mdb-react-ui-kit';
+  MDBCardLink,
+} from "mdb-react-ui-kit";
 import { useParams, useNavigate } from "react-router-dom";
-import { openContractCall } from '@stacks/connect';
-import {
-  bufferCV,
-} from '@stacks/transactions';
-import { utf8ToBytes } from '@stacks/common';
-import { userSession } from '../auth';
-import '../App.css';
+import { openContractCall } from "@stacks/connect";
+import { bufferCV } from "@stacks/transactions";
+import { utf8ToBytes } from "@stacks/common";
+import { userSession } from "../auth";
+import "../App.css";
 
-
-
-const bytes = utf8ToBytes('foo');
+const bytes = utf8ToBytes("foo");
 const bufCV = bufferCV(bytes);
 
 export default function MoviePage() {
@@ -30,11 +26,13 @@ export default function MoviePage() {
   useEffect(() => {
     const fetchMovieDetails = async () => {
       try {
-        const response = await fetch(`https://api-adad-e27e767b86bc.herokuapp.com/movies/${id}`);
+        const response = await fetch(
+          `https://api-adad-e27e767b86bc.herokuapp.com/movies/${id}`
+        );
         const data = await response.json();
         setMovieData(data);
       } catch (error) {
-        console.error('Error fetching movie details:', error);
+        console.error("Error fetching movie details:", error);
       }
     };
 
@@ -44,73 +42,89 @@ export default function MoviePage() {
   const submitMessage = async (e) => {
     e.preventDefault();
 
-    const functionArgs = [
-      bufCV
-    ];
+    const functionArgs = [bufCV];
 
     const options = {
-      contractAddress: 'ST27519QH6K6AYYJ8BGRVF778655QPRQBWYHZY26Q',
-      contractName: 'blanket-since-hint-rabbit-audit-oppose-suit-flush-relax-know-modify-voice-breeze-afraid-amazing-sell-vacant-cause-end-chaos-tilt-there-south-chicken',
-      functionName: 'test-emit-event',
+      contractAddress: "ST27519QH6K6AYYJ8BGRVF778655QPRQBWYHZY26Q",
+      contractName:
+        "blanket-since-hint-rabbit-audit-oppose-suit-flush-relax-know-modify-voice-breeze-afraid-amazing-sell-vacant-cause-end-chaos-tilt-there-south-chicken",
+      functionName: "test-emit-event",
       functionArgs,
       appDetails: {
-        name: 'Movies App Rating',
-        icon: window.location.origin + '/my-app-logo.svg',
+        name: "Movies App Rating",
+        icon: window.location.origin + "/my-app-logo.svg",
       },
-      onFinish: data => {
-        console.log('Stacks Transaction:', data.stacksTransaction);
-        console.log('Transaction ID:', data.txId);
-        console.log('Raw transaction:', data.txRaw);
+      onFinish: (data) => {
+        console.log("Stacks Transaction:", data.stacksTransaction);
+        console.log("Transaction ID:", data.txId);
+        console.log("Raw transaction:", data.txRaw);
 
         window.location.reload();
       },
     };
 
     const response = await openContractCall(options);
-
   };
 
   return (
-      <div className="movie-details-container dark-theme">
-        {/* Movie Title */}
-        <h2 className="movie-title">MOVIE</h2>
-        <p></p>
+    <div className="movie-details-container dark-theme">
+      {/* Movie Title */}
+      <h2 className="movie-title">MOVIE</h2>
+      <p></p>
 
-        {/* Movie Card */}
-        <MDBCard className="dark-card movie-card">
-          <MDBCardBody>
-            {movieData && (
-                <>
-                  {/* Movie Header with Title and Image */}
-                  <div className="movie-details-header">
-                    <MDBCardTitle><h3 className="movie-main-title">Título: {movieData.title}</h3></MDBCardTitle>
-                    <img src={movieData.photoPath} alt={movieData.title} className="movie-details-image" />
-                  </div>
-                  <p></p><p></p>
+      {/* Movie Card */}
+      <MDBCard className="dark-card movie-card">
+        <MDBCardBody>
+          {movieData && (
+            <>
+              {/* Movie Header with Title and Image */}
+              <div className="movie-details-header">
+                <MDBCardTitle>
+                  <h3 className="movie-main-title">
+                    Título: {movieData.title}
+                  </h3>
+                </MDBCardTitle>
+              </div>
+              <p></p>
+              <p></p>
 
-                  {/* Movie Details - Genre, Year, Average Score */}
-                  <div className="movie-details-details">
-                    <MDBCardSubTitle><h5>Genre: {movieData.genres}</h5></MDBCardSubTitle>
-                    <MDBCardSubTitle><h5>Year: {movieData.year || movieData.ano}</h5></MDBCardSubTitle>
-                    <MDBCardSubTitle><h5>Average Score: {movieData.avgScore}</h5></MDBCardSubTitle>
-                  </div>
+              {/* Movie Details - Genre, Year, Average Score */}
+              <div className="movie-details-details">
+                <MDBCardSubTitle>
+                  <h5>Genre: {movieData.genres}</h5>
+                </MDBCardSubTitle>
+                <MDBCardSubTitle>
+                  <h5>Year: {movieData.year || movieData.ano}</h5>
+                </MDBCardSubTitle>
+                <MDBCardSubTitle>
+                  <h5>Average Score: {movieData.avgScore}</h5>
+                </MDBCardSubTitle>
+              </div>
 
-                  {/* Movie Comments */}
-                  <div className="movie-details-comments">
-                    <h3><p>Comments:</p></h3>
-                    {movieData.comments.map((c, index) => (
-                        <MDBCardSubTitle key={index}><h5>-  {c.comment}</h5></MDBCardSubTitle>
-                    ))}
-                  </div>
-                </>
-            )}
-          </MDBCardBody>
-        </MDBCard>
+              {/* Movie Comments */}
+              {movieData.comments && movieData.comments.length > 0 && (
+                <div className="movie-details-comments">
+                  <h3>
+                    <p>Comments:</p>
+                  </h3>
+                  {movieData.comments.map((c, index) => (
+                    <MDBCardSubTitle key={index}>
+                      <h5>- {c.comment}</h5>
+                    </MDBCardSubTitle>
+                  ))}
+                </div>
+              )}
+            </>
+          )}
+        </MDBCardBody>
+      </MDBCard>
 
-        {/* Blockchain Transaction Link */}
-        {userSession.isUserSignedIn() && (
-            <a href="#" onClick={submitMessage} className="blockchain-link">Blockchain transaction</a>
-        )}
-      </div>
+      {/* Blockchain Transaction Link */}
+      {userSession.isUserSignedIn() && (
+        <a href="#" onClick={submitMessage} className="blockchain-link">
+          Blockchain transaction
+        </a>
+      )}
+    </div>
   );
 }

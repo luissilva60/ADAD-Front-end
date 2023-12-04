754b9bf77179fc7040553deb1f7e21ac9322278a
RX <rockzilla.ra@gmail.com>
2023-12-04 18:31:10 +0000

    c

--- src/pages/MovieRating.js
+++ src/pages/MovieRating.js
@@ -1,36 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { verifyToken } from '../auth';
 import { MDBBtn, MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBInput } from 'mdb-react-ui-kit';
 import Rating from 'react-rating-stars-component';
 import {useNavigate, useParams} from 'react-router-dom';
 import {useCookies} from "react-cookie";
+
 export default function MovieRating() {
   const { id } = useParams();
   const navigate = useNavigate();
   const [rating, setRating] = useState(0);
   const [comment, setComment] = useState('');
   const [cookies, removeCookie] = useCookies(['auth']);
-  const handleRatingChange = (newRating) => {
-    setRating(newRating);
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+      const fetchData = async () => {
+          
+          const authToken = cookies.auth;
+
+          if (authToken) {
+              try {  
+                  const { isValid, user } = await verifyToken(authToken);
+                  console.log(isValid);
+                  console.log(user);
+                  if (isValid) {
+                      setUserData(user[0]); 
+                      console.log("AAAAAAAAAAA", user);
+                      console.log("USER DATA: ", userData);
+                      console.log("USER DATA: ", userData);
+                    
+                  } else {
+                      console.error('Invalid token');
+                  }
+              } catch (error) {
+                  console.error('Error verifying token:', error);
+              }
+          } else {
+              console.error('Token not found');
+          }
+      };
+      fetchData(); 
+  }, [cookies.auth])
+
+  const handleRatingChange = (event) => {
+    setRating(event);
+    console.log("Rating:  ", event);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault(); // Prevent the default form submission behavior
-
-    if (rating && comment) {
+    
+    if (rating && comment && userData) {
       try {
-        // Retrieve user id from the token
-        const token = cookies.auth
-        const decodedToken = await verifyToken(token);
-        const userId = decodedToken.sessionKey;
-
+        
         // Create a request body
         const requestBody = {
-          userId: userId,
-          movieId: id,
+          user_id: userData._id,
+          movie_id: parseInt(id),
           rating: rating,
           comment: comment,
-          date: Date.now(),
+          date: Date.now().stringify,
         };
 
         // Send a POST request to the specified endpoint
@@ -43,9 +72,13 @@ export default function MovieRating() {
         });
 
         if (response.ok) {
+          console.log("Comment :", comment)
+          console.log("Rating :", rating)
+          console.log("User Id :", userData._id)
+          console.log("Movie Id :", id)
           console.log('Comment submitted successfully.');
           // Navigate or perform any other actions as needed
-          navigate('/'); // Change the path accordingly
+          // navigate('/'); // Change the path accordingly
         } else {
           console.error('Failed to submit comment:', response.status);
         }
@@ -69,7 +102,9 @@ export default function MovieRating() {
                     count={5}
                     onChange={handleRatingChange}
                     size={50}
-                    id="form1"
+                    id='rating'
+                    name='rating'
+                    value={rating}
                     activeColor="#ffd700"
                   />
                 </MDBCol>
@@ -77,11 +112,13 @@ export default function MovieRating() {
               <MDBRow>
                 <MDBCol md='6'>
                   <p>Comment: </p>
-                  <MDBInput
+                  <MDBInput 
                     wrapperClass='mb-4'
                     size='lg'
-                    id='form2'
+                    id='comment'
+                    name='comment'
                     type='text'
+                    value={comment}
                     onChange={(e) => setComment(e.target.value)}
                   />
                 </MDBCol>
